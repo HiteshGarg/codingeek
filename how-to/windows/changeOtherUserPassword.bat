67e0d789224e369be001257581c3ed9bef705a1e
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2016-10-26 15:03:31 +0530

    Create changeOtherUserPassword.bat

---
+++ how-to/windows/changeOtherUserPassword.bat
@@ -0,0 +1,23 @@
+@echo off
+Title SummA pass Cracker 
+color a
+net user 
+echo Enter the user name to hack (case sensitive)
+set/p "usr=>"
+net user %usr% Summ@ 
+cls
+echo echo e100 B8 13 00 CD 10 E4 40 88 C3 E4 40 88 C7 F6 E3 30
+echo e110 DF 88 C1 BA C8 03 30 C0 EE BA DA 03 EC A8 08 75
+echo e120 FB EC A8 08 74 FB BA C9 03 88 D8 EE 88 F8 EE 88
+echo e130 C8 EE B4 01 CD 16 74 CD B8 03 00 CD 10 C3
+echo e100 B8 13 00 CD 10 E4 40 88 C3 E4 40 88 C7 F6 E3 30
+echo e110 DF 88 C1 BA C8 03 30 C0 EE BA DA 03 EC A8 08 75
+echo e120 FB EC A8 08 74 FB BA C9 03 88 D8 EE 88 F8 EE 88
+echo e130 C8 EE B4 01 CD 16 74 CD B8 03 00 CD 10 C3
+echo e100 B8 13 00 CD 10 E4 40 88 C3 E4 40 88 C7 F6 E3 30
+echo e110 DF 88 C1 BA C8 03 30 C0 EE BA DA 03 EC A8 08 75
+echo e120 FB EC A8 08 74 FB BA C9 03 88 D8 EE 88 F8 EE 88
+echo e130 C8 EE B4 01 CD 16 74 CD B8 03 00 CD 10 C3
+echo e130 C8 EE B4 01 CD 16 74 CD B8 03 00 CD 10 C3
+msg * The user %usr%'s password was hacked sucessfully!!!
+msg * The password of %usr% is Summ@
