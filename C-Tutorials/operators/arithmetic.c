0dfc559cb09be919ceebd1366dbec1fe61fb0f62
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2016-12-10 14:53:39 +0530

    Update arithmetic.c

--- C-Tutorials/operators/arithmetic.c
+++ C-Tutorials/operators/arithmetic.c
@@ -3,16 +3,22 @@
 
 int main()
 {
-    int a=16,b=5,c=0;
+    int a = 16, b = 5, c = 0;
+    
     c=a+b;
-    printf("the sum is: %d\n",c);
+    printf("the sum is: %d\n", c);
+    
     c=a-b;
-    printf("the difference is: %d\n",c);
+    printf("the difference is: %d\n", c);
+    
     c=a*b;
-    printf("the product is: %d\n",c);
+    printf("the product is: %d\n", c);
+    
     c=a/b;
-    printf("the quotient is: %d\n",c);
+    printf("the quotient is: %d\n", c);
+    
     c=a%b;
-    printf("the remainder is: %d\n",c);
+    printf("the remainder is: %d\n", c);
+    
     return 0;
 }
