fc82d897a49c844dcb0dd6f34e03400fd0aefcb0
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2016-12-10 15:05:43 +0530

    Update Bitwise.c

--- C-Tutorials/operators/Bitwise.c
+++ C-Tutorials/operators/Bitwise.c
@@ -3,18 +3,25 @@
 
 int main()
 {
-    int a=5,b=16,c;
-    c=a&b;
-    printf("The Bitwise AND: %d\n",c);
-    c=a|b;
-    printf("The Bitwise OR: %d\n",c);
-    c=a^b;
-    printf("The Bitwise Exclusive OR: %d\n",c);
-    c=~b;
-    printf("The Bitwise Complement: %d\n",c);
-    c=b>>2;
-    printf("The Bitwise right shift: %d\n",c);
-    c=a<<3;
-    printf("The Bitwise left shift: %d\n",c);
+    int a=5, b=16, c;
+    
+    c = a & b;
+    printf("The Bitwise AND: %d\n", c);
+    
+    c = a|b;
+    printf("The Bitwise OR: %d\n", c);
+    
+    c = a ^ b;
+    printf("The Bitwise Exclusive OR: %d\n", c);
+    
+    c = ~b;
+    printf("The Bitwise Complement: %d\n", c);
+    
+    c = b >> 2;
+    printf("The Bitwise right shift: %d\n", c);
+    
+    c = a << 3;
+    printf("The Bitwise left shift: %d\n", c);
+    
     return 0;
 }
