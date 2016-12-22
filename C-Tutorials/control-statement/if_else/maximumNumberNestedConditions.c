fa206f159655c5c817d90836ef561cf20f5b94ce
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2016-12-22 22:24:29 +0530

    Update and rename C-Tutorials/if_else/max3.c to C-Tutorials/control-statement/if_else/maximumNumberNestedConditions.c

--- C-Tutorials/control-statement/if_else/maximumNumberNestedConditions.c
+++ C-Tutorials/control-statement/if_else/maximumNumberNestedConditions.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+
 int main()
 {
     int a=5,b=16,c=20;
     if(a>b)
     {
-        if(a>c)
+        if(a>c){
          printf("The greater number is : %d",a);
-        else
+        }
+        else{
          printf("The greater number is : %d",c);
+        }
     }
     else
     {
       if(b>c)
-      printf("The greater number is : %d",b);
+      	printf("The greater number is : %d",b);
       else
-      printf("The greater number is : %d",c);
+      	printf("The greater number is : %d",c);
     }
 
     return 0;
