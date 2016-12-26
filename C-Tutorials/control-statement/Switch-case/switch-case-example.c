b435d64c8085c4846a7cfcf113a219ae3df5ac11
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2016-12-26 12:36:36 +0530

    Update switch-case-example.c

--- C-Tutorials/control-statement/Switch-case/switch-case-example.c
+++ C-Tutorials/control-statement/Switch-case/switch-case-example.c
@@ -7,26 +7,26 @@ int main()
     printf("Enter 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division :\n");
     scanf("%d",&ch);
     
-	switch(ch)
+    switch(ch)
     {
-		case 1:
-			result=16+5;
-			printf("The sum is: %f \n",result);
-			break;
-		case 2:
-			result=16-5;
-			printf("The difference is: %f \n",result);
-			break;
-		case 3:
-			result=16*5;
-			printf("The product is: %f \n",result);
-			break;
-		case 4:
-			result=16/5;
-			printf("The quotient is: %f \n",result);
-			break;
-		default:
-			printf("Wrong Choice\n");
+        case 1:
+            result=16+5;
+            printf("The sum is: %f \n",result);
+            break;
+        case 2:
+            result=16-5;
+            printf("The difference is: %f \n",result);
+            break;
+        case 3:
+            result=16*5;
+            printf("The product is: %f \n",result);
+            break;
+        case 4:
+            result=16/5;
+            printf("The quotient is: %f \n",result);
+            break;
+        default:
+            printf("Wrong Choice\n");
     }
     return 0;
 }
