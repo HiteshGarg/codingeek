96a9a6bc0297a9dd47fdba51b117ec49b003c950
Hitesh Kumar Garg <hiteshkumargarg@gmail.com>
2017-01-08 17:36:56 +0530

    new nested while loop file

---
+++ C-Tutorials/loops/while_do_while/nestedWhileError.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i=1,j;
+    while(i<=3)
+    {
+        j=1;
+        while(j<=2)
+        {
+            printf("CodinGeek\n");
+            j++;
+        }
+        printf("-----\n");
+        i++;
+    }
+    return 0;
+}
