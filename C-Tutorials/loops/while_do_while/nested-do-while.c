f2b746b22052547f67c2f1daf4f39ab9c5331bfe
Hitesh Kumar Garg <hiteshkumargarg@gmail.com>
2017-01-08 17:48:14 +0530

    nested do while

---
+++ C-Tutorials/loops/while_do_while/nested-do-while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i=1,j;
+	do{
+		j=1;
+		do{
+			printf("CodinGeek\n");
+		}while(++j<=2);
+		
+		printf("-----\n");
+
+	}while(++i<=3);
+    
+	return 0;
+}
