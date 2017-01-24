06b9b6aab3a7141df22a2d5755327bc5eb126453
NishirikaKumari <nishirikaK@gmail.com>
2017-01-24 11:26:37 +0530

    Create recursive_program.c

---
+++ C-Tutorials/recursion/recursive_program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void recursion(int n)
+{
+    if(n<=3){ //exit condition for recursive function
+        printf("%d\n",n);
+        recursion(++n); //calling itself again
+    }
+    else
+        return ;
+
+}
+int main()
+{
+    int x=1;
+    recursion(x);
+    return 0;
+}
