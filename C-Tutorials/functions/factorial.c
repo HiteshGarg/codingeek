e65bcf400aa9a0ca5747444b75475d875980a151
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-01-29 22:56:07 +0530

    Update factorial.c

--- C-Tutorials/functions/factorial.c
+++ C-Tutorials/functions/factorial.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 long fact(int);//function declaration
+
 int main()
 {
     int i,n;
@@ -7,18 +9,21 @@ int main()
     printf("Enter the range:\n");
     scanf("%d",&n);
     //each number is sent to the function fact() and it returns the factorial of the number
-    for(i=1;i<=n;i++){
+    
+    for(i=1; i<=n; i++){
         //we are using call by value
        f=fact(i);//actual parameter
        printf("the factorial of %d is: %d\n",i,f);//prints the factorial of each number
     }
     return 0;
 }
+
 long fact(int x)//function definition with formal parameter
 {
     int i;
     long f=1;
-    for(i=1;i<=x;i++)
+    
+    for(i=1; i<=x; i++)
     {
         f=f*i;//calculates factorial of the number
     }
