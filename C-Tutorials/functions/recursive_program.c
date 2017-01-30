10f6bdbecc6b24dc60325f6468a91f859c6e3c83
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-01-30 14:24:43 +0530

    Update and rename C-Tutorials/recursion/recursive_program.c to C-Tutorials/functions/recursive_program.c

--- C-Tutorials/functions/recursive_program.c
+++ C-Tutorials/functions/recursive_program.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 void recursion(int n)
 {
-    if(n<=3){ //exit condition for recursive function
+    if( n<=3 ){ //exit condition for recursive function
         printf("%d\n",n);
         recursion(++n); //calling itself again
     }
-    else
+    else{
         return ;
-
+    }
 }
+
 int main()
 {
     int x=1;
