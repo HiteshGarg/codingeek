e9ed9cbde5d9c89d25b93229458df0f8f58967d8
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-02-28 01:28:29 +0530

    Update CollectionsAddAllExample.java

--- Java/conversions/arrayToList/CollectionsAddAllExample.java
+++ Java/conversions/arrayToList/CollectionsAddAllExample.java
@@ -9,19 +9,19 @@ import java.util.stream.Collectors;
 public class ArrayToListConversion {
 
       //Collections.addAll()
-//    public static void main(String[] args) {
-//        final String arr[] = new String[] { "G", "E", "E", "K" };
-//        final List<String> initialList = new ArrayList<String>() {{
-//            add("C");
-//            add("O");
-//            add("D");
-//            add("I");
-//            add("N");
-//        }};
-//
-//        // Elements of the array are appended at the end
-//        Collections.addAll(initialList, arr);
-//        System.out.println(initialList);
-//    }
+   public static void main(String[] args) {
+       final String arr[] = new String[] { "G", "E", "E", "K" };
+       final List<String> initialList = new ArrayList<String>() {{
+           add("C");
+           add("O");
+           add("D");
+           add("I");
+           add("N");
+       }};
+
+       // Elements of the array are appended at the end
+       Collections.addAll(initialList, arr);
+       System.out.println(initialList);
+   }
 
 }
