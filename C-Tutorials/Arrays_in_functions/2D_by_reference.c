bfa046c1ac9c8e0dd3d2b0ed094d3af3a53094f9
NishirikaKumari <nishirikaK@gmail.com>
2017-03-03 09:24:59 +0530

    Create 2D_by_reference.c

---
+++ C-Tutorials/Arrays_in_functions/2D_by_reference.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void display(int *a[2][2])//the formal parameter takes a sized array as argument
+{
+    int i,j;
+    printf("The matrix is:\n");//the matrix is displayed using two for loops
+    for(i=0 ;i<2 ;i++)
+    {
+        for(j=0 ;j<2 ;j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+}
+int main()
+{
+    int num[2][2],i,j;
+    int *x;//declaring a pointer to store the address of the array.
+    printf("Enter 2x2 numbers:\n");
+    for(i=0 ;i<2 ;i++){
+        for(j=0 ;j<2 ;j++){
+            scanf("%d",&num[i][j]);
+        }
+    }
+    x=&num;//storing the address of the array
+    display(x);//passing by reference-sending the address of the array.
+}
+
