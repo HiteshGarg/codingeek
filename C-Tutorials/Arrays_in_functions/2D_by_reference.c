e494b223c9cc25dfaa781684ad80b395f7fc28e0
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-03-05 13:30:42 +0530

    Update 2D_by_reference.c

--- C-Tutorials/Arrays_in_functions/2D_by_reference.c
+++ C-Tutorials/Arrays_in_functions/2D_by_reference.c
@@ -1,28 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
 void print( int ** arr , int row, int col ){
-    int i , j;
-    for ( i = 0; i < row; i++) {
-            for( j = 0; j < col; j++)
-                    printf("%d " , arr[i][j]);
-            printf("\n");
-    }
+	int i , j;
+	printf("\nThe matrix is:\n");
+	
+	for ( i = 0; i < row; i++) {
+		for( j = 0; j < col; j++)
+			printf("%d " , arr[i][j]);
+		printf("\n");
+	}
 }
 int main() {
-    int row, col, i , j;
-    scanf("%d" , &row);
-    scanf("%d" , &col);
-    int **arr = (int **) malloc( sizeof( int*) * row);
-    for( i = 0; i < row; i++) {
-            arr[i] = (int * ) malloc( sizeof( int ) * col );
-    }
-    //now Scan or initialise
-    for( i = 0; i < row; i++) {
-            for( j = 0; j < col; j++)
-                    scanf("%d" , &arr[i][j]);
-    }
-    // pass the array to function print as 
-    print ( arr ,  row, col);
-    return 0;
+	int row, col, i , j;
+	
+	printf("Enter number of rows - ");
+	scanf("%d" , &row);
+	printf("\nEnter number of columns - ");
+	scanf("%d" , &col);
+	int **arr = (int **) malloc( sizeof( int*) * row);
+	for( i = 0; i < row; i++) {
+			arr[i] = (int * ) malloc( sizeof( int ) * col );
+	}
+	printf("\nEnter %d * %d array\n", row, col);
+	//now Scan or initialise
+	for( i = 0; i < row; i++) {
+		for( j = 0; j < col; j++){
+				scanf("%d" , &arr[i][j]);
+		}
+	}
+	// pass the array to function print as 
+	print ( arr ,  row, col);
+	return 0;
 }
  
