1ca91d8dbc19ee75735f91fa4afe17c6aa8c0231
NishirikaKumari <nishirikaK@gmail.com>
2017-04-08 10:15:53 +0530

    Create structure_pointer.c

---
+++ C-Tutorials/structure_features/structure_pointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*declaring the structure*/
+struct student{
+    char name[10];
+    int id;
+    double marks;
+};
+void display(struct student *);//declaring the function to display the contents of the structure address passed to the function
+
+int main()
+{
+    struct student s1={"Ram",101,79.0};
+    struct student s2={"Mohan",102,99.0};
+    struct student *x;
+    x=&s1;//storing the address of the structure variable s1
+    /*displaying the details of student1 using pointers*/
+    printf("The student details are as follows:\n");
+    printf(" Name= %s\n ID= %d\n marks= %f\n\n",x->name,x->id,x->marks);
+    display(&s2);//sending the address of the structure variable s2 to function display()
+    return 0;
+}
+/*displaying the contents of the address sent to the function*/
+void display(struct student *s)
+{
+    printf("The student details are as follows:\n");
+    printf(" Name= %s\n ID= %d\n marks= %f\n\n",s->name,s->id,s->marks);
+}
