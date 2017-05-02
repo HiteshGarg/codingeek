a570fac197e25103cc20799186f3d8d9ff2007ef
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-05-02 23:34:04 +0530

    Update and rename C-Tutorials/structure_features/equate.c to C-Tutorials/structures/CopyValueWithEqualsOperator.c

--- C-Tutorials/structures/CopyValueWithEqualsOperator.c
+++ C-Tutorials/structures/CopyValueWithEqualsOperator.c
@@ -7,12 +7,15 @@ int main()
         int id;
         float marks;
     };
+    
     struct student s1={"Ram",101,79.0};//we store the details of the first student
     struct student s2,s3;
+    
     /*the piece meal copying or copying each element separately*/
     strcpy(s2.name,s1.name);
     s2.id=s1.id;
     s2.marks=s1.marks;
+    
     /*copying the elements directly using '=' operator*/
     s3=s1;
     /*Displaying the copied details*/
