17c9754aba06f2ec97267126b781c9f4c0d41993
NishirikaKumari <nishirikaK@gmail.com>
2017-05-08 00:50:39 +0530

    Create string_io_files.c

---
+++ C-Tutorials/Files/string_io_files.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+   FILE *fp,*fw;//file pointers for the respective read and write mode.
+   char s[30];
+
+   fp=fopen("E://Doc1.txt","r");//The file that contains the String
+   fw=fopen("E://Doc2.txt","w");//The file from which the string is being copied
+
+   /*while runs until it gets NULL*/
+   while(fgets(s,30,fp)!=NULL) { //reading the string from the file Doc1.txt
+    fputs(s,fw);//copying to the file Doc2.txt
+    printf("%s",s);
+   }
+   fclose(fp);
+   fclose(fw);
+   return 0;
+}
