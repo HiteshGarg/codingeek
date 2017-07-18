0e5154b65b3e0c7a76c4de0895ead27096d50ee6
Hitesh Garg <hitesh.garg@nagarro.com>
2017-07-18 14:13:23 +0530

    Formatting for each post

--- Java/ForEachLoop/ForEachItr.java
+++ Java/ForEachLoop/ForEachItr.java
@@ -1,18 +1,25 @@
-/*Program to print the elements in the array before and after making changes to the iteration variable*/
-
+/**
+ * Program to print the elements in the array before and after making changes to the iteration variable.
+ */
 public class ForEachItr {
-	public static void main(String args[]) {
-		int num[]={3,6,1,4,9,7,4,6,2,8};
-		/*the for-each loop: integer type data with n as the iteration variable used to access the array num[]*/
-		/*Original array elements*/
-		System.out.println("The original array elements are : ");
-		for(int n:num) 
-			System.out.print(n+" ");
-		/*Changes made to n*/
-		System.out.println("\nArray elements after changes being made to n : ");
-		for(int n:num) {
-			System.out.print(n+" ");
-			n+=2;
-		}
-	}
+
+    public static void main(String args[]) {
+        int num[] = { 3, 6, 1, 4, 9, 7, 4, 6, 2, 8 };
+
+        // the for-each loop: integer type data with n as the iteration variable used to access the array num[]
+        // Original array elements
+        System.out.println("The original array elements are : ");
+
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+
+        // Changes made to n
+        System.out.println("\nArray elements after changes being made to n : ");
+
+        for (int n : num) {
+            System.out.print(n + " ");
+            n += 2;
+        }
+    }
 }
--- Java/ForEachLoop/ForEachMultiDim.java
+++ Java/ForEachLoop/ForEachMultiDim.java
@@ -1,15 +1,20 @@
-/*Program to print the elements in a 2D array*/
-
+/**
+ * Program to print the elements in a 2D array
+ */
 public class ForEachMultiDim {
-	public static void main(String args[]) {
-		int num[][]={{3,6,1},{4,9,7},{4,6,2}};
-		/*the for-each loop: integer type data with n[] as the iteration variable used to access the array num[][]*/
-		for(int n[]:num) {
-			/*The inner loop with iteration variable x used to access the multiple 1D arrays accessed by n[]*/
-			for(int x:n) {
-				System.out.print(x+" ");
-			}
-			System.out.println();
-		}
-	}
+
+    public static void main(String args[]) {
+
+        int num[][] = { { 3, 6, 1 }, { 4, 9, 7 }, { 4, 6, 2 } };
+
+        //the for-each loop: integer type data with n[] as the iteration variable used to access the array num[][]
+        for (int n[] : num) {
+
+            //The inner loop with iteration variable x used to access the multiple 1D arrays accessed by n[]
+            for (int x : n) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
 }
