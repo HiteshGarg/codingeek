68dc9756010907ac541c7556a1b5c1a7149835d9
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-07-23 23:26:11 +0530

    Update TestContinueLabel.java

--- Java/BasicJumpStmt/TestContinueLabel.java
+++ Java/BasicJumpStmt/TestContinueLabel.java
@@ -4,7 +4,7 @@ public class TestContinueLabel {
 	public static void main(String args[]) {
 		int i,j;
 		outer:
-			for(i=0 ;i<10 ;i++ ) {
+			for(i=0 ; i<10 ;i++ ) {
 				for(j=0 ;j<10 ;j++ ) {
 					if(j>i) {
 						System.out.println();
