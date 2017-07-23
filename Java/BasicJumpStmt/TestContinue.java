55c46a128495ecbc01fed1e81cd3fb32a6c4d3ce
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-07-23 23:25:42 +0530

    Update TestContinue.java

--- Java/BasicJumpStmt/TestContinue.java
+++ Java/BasicJumpStmt/TestContinue.java
@@ -1,8 +1,10 @@
-/*This program shows the use of continue in loops. It prints the odd numbers in a pattern and skips the even numbers.*/
+/*This program shows the use of continue in loops.
+It prints the odd numbers in a pattern and skips the even numbers.*/
 
 public class TestContinue {
 	public static void main(String args[]) {
 		int i,j;
+		
 		for(i=1; i<=10 ;i++ ) {
 			if(i%2==0)
 				continue; //skips all the even iterations
