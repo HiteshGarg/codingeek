8f74f12ac7661e8402b4ab2f608d8a458d3cb996
Hitesh Garg <hitesh.garg@nagarro.com>
2017-07-30 01:05:05 +0530

    Autobox Autounbox

--- Java/Autobox_Unbox/AutoboxMixExpDemo.java
+++ Java/Autobox_Unbox/AutoboxMixExpDemo.java
@@ -1,15 +1,21 @@
-/*The following program shows the use of objects in mixed expressions thus demonstrating Autoboxing and Auto-unboxing*/
+/**
+ * The following program shows the use of objects in mixed
+ * expressions thus demonstrating Autoboxing and Auto-unboxing
+ */
 
 public class AutoboxMixExpDemo {
-	public static void main(String args[]) {
-		Integer iOb = 66; //creating Integer object with value 56
-		Double dOb = 30.0; ////creating Double object with value 30.0
-		System.out.println("Value of iOb = " + iOb);
-		System.out.println("Value of dOb = " + dOb);
-		
-		/*Operating on Integer and Double objects which are first unboxed to perform the operation and then the value is boxed and stored into dOb*/
-		dOb = iOb / dOb;
-		System.out.println("Final Value of dOb = " + dOb);
-		
-	}
+    public static void main(String args[]) {
+        //creating Integer object with value 56
+        Integer iOb = 66;
+        //creating Double object with value 30.0
+        Double dOb = 30.0;
+        System.out.println("Value of iOb = " + iOb);
+        System.out.println("Value of dOb = " + dOb);
+
+        //Operating on Integer and Double objects which are first unboxed
+        //to perform the operation and then the value is boxed and stored into dOb
+        dOb = iOb / dOb;
+        System.out.println("Final Value of dOb = " + dOb);
+
+    }
 }
