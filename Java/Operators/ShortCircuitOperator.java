8597d3851514f4052822419c70d124a934d978bc
Hitesh Garg <hitesh.garg@nagarro.com>
2017-07-30 01:15:08 +0530

    Formatting

---
+++ Java/Operators/ShortCircuitOperator.java
@@ -0,0 +1,24 @@
+/**
+ * The following program shows the use of the secondary Logical
+ * AND and OR(Short-circuit operators)
+ */
+
+public class ShortCircuitOperator {
+    public static void main(String args[]) {
+        int denom = 0, n = 5;
+        //The right part of the if expression will show a arithmetic exception
+        // when used with & but in this case it does not because && derives the
+        // result by only looking at the left part of the expression
+        if (denom != 0 && n / denom > 10) {
+            System.out.println(n / denom);
+        } else {
+            System.out.println("denom is zero");
+        }
+
+        //Demonstration of the working of logical || operator
+        int a = 9, b = 10;
+        if (a > 5 || b++ != 0) {
+            System.out.println(b);//this statement will print 10 despite the increment operation performed in the if statement.
+        }
+    }
+}
---
+++ Java/WrapperClass/IntLongDemo.java
@@ -0,0 +1,20 @@
+/**
+ * This program shows the use of Integer and Long wrapper classes and some of its functions
+ */
+
+public class IntLongDemo {
+
+    public static void main(String args[]) {
+        //Wrapping the int value 100 into the object i1
+        Integer i1 = new Integer(100);
+        //Wrapping the int value 15 into the object i2
+        Integer i2 = new Integer(15);
+        //l is assigned the long value 4567
+        Long l = new Long(4567);
+
+		//Some of the methods are shown below
+        System.out.println("Sum of " + i1 + " and " + i2 + " is : " + Integer.sum(i1, i2));
+        System.out.println("The maximum of the two Integer objects is : " + Integer.max(i1, i2));
+        System.out.println("The octal of the number " + l + " is : " + Long.toOctalString(l));
+    }
+}
