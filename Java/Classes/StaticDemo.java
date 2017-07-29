6dd10ed22dc145e2a43ab53f510930c8b0eea30a
NishirikaKumari <nishirikaK@gmail.com>
2017-07-29 22:37:30 +0530

    Create StaticDemo.java

---
+++ Java/Classes/StaticDemo.java
@@ -0,0 +1,18 @@
+/*
+ *This program shows the creation of static nested class. 
+ */
+ class StaticDemo {
+	 
+	 static class NestedClass {					//creation of a static nested class
+		 
+		 void meth() {
+		 System.out.println("Inside static Inner class");
+		 
+		 }
+	 }
+	 
+	 public static void main(String args[]) {
+		 NestedClass ob = new NestedClass();	//object to access the static nested class
+		 ob.meth();								//calls the method inside the static nested class
+	 }
+ }
