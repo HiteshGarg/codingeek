3baa8b8939420c82d58d5eb1b4c70068f4594baf
smrithi7 <k.srimathi7@gmail.com>
2017-08-07 22:32:12 +0530

    Create multi-mod.sql

---
+++ SQL/Basic/create-command/multi-mod.sql
@@ -0,0 +1,6 @@
+ALTER TABLE table_name
+	MODIFY (column_name1 column_datatype[(size)],
+          column_name2 column_datatype[(size)],
+          column_name3 column_datatype[(size)],
+          ....
+    );
