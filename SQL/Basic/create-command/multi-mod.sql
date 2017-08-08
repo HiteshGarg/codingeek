4550b8891918585139c6775006f14e42ddb3da76
smrithi7 <k.srimathi7@gmail.com>
2017-08-08 22:47:51 +0530

    Update multi-mod.sql

--- SQL/Basic/create-command/multi-mod.sql
+++ SQL/Basic/create-command/multi-mod.sql
@@ -1,6 +1,15 @@
+-- SYNTAX
+
 ALTER TABLE table_name
 	MODIFY (column_name1 column_datatype[(size)],
           column_name2 column_datatype[(size)],
           column_name3 column_datatype[(size)],
           ....
+    );
+
+
+-- EXAMPLE
+ALTER TABLE codingeeksql
+	MODIFY (ID SMALLINT,
+          NAME CHAR(13)
     );
