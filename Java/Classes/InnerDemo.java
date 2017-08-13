2cb348ded39648b3ab17d90b8ac7018de1e6d4bb
Hitesh Garg <hitesh.garg@nagarro.com>
2017-08-13 17:55:59 +0530

    Formatting

--- Java/Classes/InnerDemo.java
+++ Java/Classes/InnerDemo.java
@@ -1,24 +1,28 @@
 /*
  *This program defines a private inner class 
  */
- class Outer {
-	 
-	 private class Inner {				//declaring a private inner class
-		 
-		 public void meth() {
-			 System.out.println("Inside inner class");	
-		 }
-	 }
-	 
-	 public void meth2() {
-		 Inner ob = new Inner();		//creating an object to access the inner class
-		 ob.meth();
-	 }
- }
- public class InnerDemo {
-	 
-	 public static void main(String args[]) {
-		 Outer obj = new Outer();		//creating object to access the outer class 
-		 obj.meth2();					      //calling meth2() which in turn calls the inner class
-	 }
- }
+class Outer {
+
+    public void meth2() {
+        //creating an object to access the inner class
+        Inner ob = new Inner();
+        ob.meth();
+    }
+
+    //declaring a private inner class
+    private class Inner {
+        public void meth() {
+            System.out.println("Inside inner class");
+        }
+    }
+}
+
+public class InnerDemo {
+
+    public static void main(String args[]) {
+        //creating object to access the outer class
+        Outer obj = new Outer();
+        //calling meth2() which in turn calls the inner class
+        obj.meth2();
+    }
+}
