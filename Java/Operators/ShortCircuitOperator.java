e13107df3aa79812485c7938e9a151b2d8af244e
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-08-13 15:17:30 +0530

    Update ShortCircuitOperator.java

--- Java/Operators/ShortCircuitOperator.java
+++ Java/Operators/ShortCircuitOperator.java
@@ -1,24 +1,44 @@
-/**
- * The following program shows the use of the secondary Logical
- * AND and OR(Short-circuit operators)
+package com.codingeek.java.operators;
+
+/*
+ *The following program shows the use of the secondary Logical AND and OR(Short-circuit operators)
  */
-
 public class ShortCircuitOperator {
-    public static void main(String args[]) {
-        int denom = 0, n = 5;
-        //The right part of the if expression will show a arithmetic exception
-        // when used with & but in this case it does not because && derives the
-        // result by only looking at the left part of the expression
-        if (denom != 0 && n / denom > 10) {
-            System.out.println(n / denom);
-        } else {
-            System.out.println("denom is zero");
-        }
-
-        //Demonstration of the working of logical || operator
-        int a = 9, b = 10;
-        if (a > 5 || b++ != 0) {
-            System.out.println(b);//this statement will print 10 despite the increment operation performed in the if statement.
-        }
-    }
-}
+	 
+	 public static void main(String args[]) {
+		 int a = 3, b = 5, c = 0;
+		 
+		 //the || operator only checks the left hand side, so the value of b remains unchanged
+		 if(a != 0 || b++ > 0) {
+			 System.out.println("(a != 0 || b++ > 0) is true and value of b is : " + b);
+		 } else {
+			 System.out.println("(a != 0 || b++ > 0) is false");
+		 }
+		 
+		 System.out.println();
+		 
+		 //the | operator checks both the operands and increments the value of b
+		 if(a != 0 | b++ > 0) {
+			 System.out.println("(a != 0 | b++ > 0) is true and value of b is : " + b);
+		 } else {
+			 System.out.println("(a != 0 | b++ > 0) is false");
+		 }
+		 
+		 System.out.println();
+		 
+		 //The && operator only checks the left hand side thus it does not reveal any exceptions
+		 if(a != 3 && (a / c) > 5) {
+			 System.out.println("(a != 3 && (a / c) > 5) is true");
+		 } else {
+			 System.out.println("(a != 3 && (a / c) > 5) is false");
+		 }
+		 System.out.println();
+		 
+		 //the & operator checks both the operands and it reveals exception in the thread
+		 if(a != 3 & (a / c) > 5) {
+			 System.out.println("(a != 3 & (a / c) > 5) is true");
+		 } else {
+			 System.out.println("(a != 3 & (a / c) > 5) is false"); 
+		 }
+	}
+ }
