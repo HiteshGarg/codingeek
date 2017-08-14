3cf12a13298f369143e68cbcd0dc90204c744820
smrithi7 <k.srimathi7@gmail.com>
2017-08-14 13:07:19 +0530

    Update update-syn.sql

--- SQL/Basic/create-command/update-syn.sql
+++ SQL/Basic/create-command/update-syn.sql
@@ -1,3 +1,11 @@
+--SYNTAX:
+
 UPDATE table_name
 SET column1 = value1, column2 = value2,...valueN
 WHERE condition;
+
+
+--EXAMPLE:
+UPDATE Users
+SET FirstName = 'Catherine', City= 'Mumbai'
+WHERE CustomerID = 3;
