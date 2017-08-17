587d46e38004576b2c2db117d9a4bae9ff7075f2
Hitesh Garg <hitesh.garg@nagarro.com>
2017-08-18 00:09:03 +0530

    Formatting Space

--- Java/Classes/AnonymousClassDemo.java
+++ Java/Classes/AnonymousClassDemo.java
@@ -3,25 +3,25 @@ package com.codingeek.java.InnerClass;
 /*
  * This program shows a simple anonymous class
  */
- public class AnonymousClassDemo {
-	 void meth1(){
-	 }
-	 
-	 public static void main(String args[]) {
-		 
-		 //the anonymous class is present after the object declaration
-		 AnonymousClassDemo ob = new AnonymousClassDemo() {
-			 
-			 void meth1() {
-				 meth2();
-			 }
-			 
-			 void meth2() {
-				 System.out.println("Inside Anonymous class");
-			 }
-		 };
-		 
-		 //calling meth1() which in turn calls meth2() in its body.
-		 ob.meth1();
-	 }
- }
+public class AnonymousClassDemo {
+    public static void main(String args[]) {
+
+        //the anonymous class is present after the object declaration
+        AnonymousClassDemo ob = new AnonymousClassDemo() {
+
+            void meth1() {
+                meth2();
+            }
+
+            void meth2() {
+                System.out.println("Inside Anonymous class");
+            }
+        };
+
+        //calling meth1() which in turn calls meth2() in its body.
+        ob.meth1();
+    }
+
+    void meth1() {
+    }
+}
