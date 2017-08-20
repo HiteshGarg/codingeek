4f8a659eb66368b5815f75ac787d4cf3adf04a6a
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-08-20 17:23:55 +0530

    Update and rename SQL/Basic/create-command/insert-nested.sql to SQL/Basic/insert-command/insert-nested.sql

--- SQL/Basic/create-command/insert-nested.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO codingeek
-SELECT * FROM  tutorials
-WHERE Preferred_subject = ANY(
-SELECT Preferred subject FROM lecture
-WHERE Availability == 'open'));
---
+++ SQL/Basic/insert-command/insert-nested.sql
@@ -0,0 +1,6 @@
+INSERT INTO codingeek
+SELECT * FROM  tutorials
+WHERE 
+Preferred_subject = 
+  ANY( SELECT Preferred subject FROM lecture
+    WHERE Availability == 'open'));
