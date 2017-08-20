8d85d12c17868d2327c98df02c1068c37d2444c1
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-08-20 17:27:13 +0530

    Update insert-type1.sql

--- SQL/Basic/insert-command/insert-type1.sql
+++ SQL/Basic/insert-command/insert-type1.sql
@@ -1,4 +1,9 @@
---SYNTAX
+--SYNTAX Example 1 - Inserting without mentioning the column names
+
+INSERT INTO 
+Table_name VALUES (value1,value2,value3,...valueN);
+
+--SYNTAX Example 2 - Inserting for specific columns
 
 INSERT INTO 
 Table_name(column1, column2, column3,...columnN)  
