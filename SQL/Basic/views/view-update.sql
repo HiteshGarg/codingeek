41901a67f7452d5571e3d2b25878a8a27f943bfa
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2017-09-02 22:56:51 +0530

    Update view-update.sql

--- SQL/Basic/views/view-update.sql
+++ SQL/Basic/views/view-update.sql
@@ -1,4 +1,4 @@
-CREATE VIEW Student_view AS
+CREATE OR REPLACE VIEW Student_view AS
 SELECT Student_Id.ID, student.Name, student.Grade, student.Points
 FROM  Student_Id,student;
 WHERE student.Age > 13
