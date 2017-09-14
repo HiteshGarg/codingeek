cd5836688cd53c751eaf06f90f6bf7848bc4aa47
smrithi7 <k.srimathi7@gmail.com>
2017-09-14 20:53:48 +0530

    Update three.sql

--- SQL/Basic/alias/three.sql
+++ SQL/Basic/alias/three.sql
@@ -1,7 +1,7 @@
 SELECT Subject AS Sub_name, Code AS Sub_ID
-FROM marks
-WHERE Pass_marks >= 40
+FROM marks AS m
+WHERE m.Pass_marks >= 40
 UNION ALL
 SELECT Subject AS Sub_name, Code AS Sub_ID
-FROM language
-WHERE High_score > 85;
+FROM language AS l
+WHERE l.High_score > 85;
