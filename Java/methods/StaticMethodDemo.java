2f3d6c45332aff26be95c12eed7dc9c43e7a3588
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2018-01-06 12:41:17 +0530

    Update StaticMethodDemo.java

--- Java/methods/StaticMethodDemo.java
+++ Java/methods/StaticMethodDemo.java
@@ -1,20 +1,20 @@
 /**
- *This program shows the use of static methods.
+ * This program shows the use of static methods.
  */
-  
-  package com.codingeek.java.Methods;
-  
-  public class StaticMethodDemo {
-	  
-	 /**
-	  * The following method is defined as static so it can be
-		* called by the main() method directly since main() method is also declared as static
-	  */
-	  static void meth() {
-		  System.out.println("Inside the static method meth()");
-	  }
-	  
-	  public static void main(String[] args) {
-		  meth(); //calling the static method meth()
-	  }
-  }
+
+package com.codingeek.java.Methods;
+
+public class StaticMethodDemo {
+
+    /**
+     * The following method is defined as static so it can be
+     * called by the main() method directly since main() method is also declared as static
+     */
+    static void meth() {
+        System.out.println("Inside the static method meth()");
+    }
+
+    public static void main(String[] args) {
+        meth(); //calling the static method meth()
+    }
+}
