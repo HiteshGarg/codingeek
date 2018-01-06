1730de53e595db64c4df79e7736aa6638e65b71d
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2018-01-06 12:39:49 +0530

    Update RecursionDemo.java

--- Java/methods/RecursionDemo.java
+++ Java/methods/RecursionDemo.java
@@ -1,36 +1,40 @@
 /**
-  *The following program prints the factorial of a number in the range specified by the user.
-  *This program uses recursion.
-  */
-  
-  package com.codingeek.java.Methods;
-  import java.util.Scanner;
-  
-  public class RecursionDemo {
-	  
-	  /*
-	   *This is the recursive method which calculates the factorial of a number.
-	   */
-	   int calcFactorial(int x) {
-		   
-		   if(x == 0 || x == 1)
-			   return 1;
-		   else
-			   return x * calcFactorial(x-1); //decreases the value of x for every call
-	   }
-	   
-	   public static void main(String[] args) {
-		   
-		   Scanner in = new Scanner(System.in);
-		   RecursionDemo ob = new RecursionDemo();
-		   
-		   int a, b;
-		   System.out.println("Enter the range :");
-		   a = in.nextInt();
-		   b = in.nextInt();
-		   
-		   for(int i = a; i <= b; i++) 
-				 //calling the function calcFactorial() to calculate the factorial of the numbers individually 
-			   System.out.println("The factorial of " + i + " = " +ob.calcFactorial(i)); 
-	   }
-  }
+ * The following program prints the factorial of a number in the range specified by the user.
+ * This program uses recursion.
+ */
+
+package com.codingeek.java.Methods;
+
+import java.util.Scanner;
+
+public class RecursionDemo {
+
+    /*
+     *This is the recursive method which calculates the factorial of a number.
+     */
+    int calcFactorial(int x) {
+
+        if (x == 0 || x == 1) {
+            return 1;
+        } else {
+            return x * calcFactorial(x - 1); //decreases the value of x for every call
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        RecursionDemo ob = new RecursionDemo();
+
+        int a, b;
+        System.out.println("Enter the range :");
+        a = in.nextInt();
+        b = in.nextInt();
+
+        for (int i = a; i <= b; i++)
+        //calling the function calcFactorial() to calculate the factorial of the numbers individually 
+        {
+            System.out.println("The factorial of " + i + " = " + ob.calcFactorial(i));
+        }
+    }
+}
