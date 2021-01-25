6a79abf473c8343a6c7cae71c463114d496e3d3b
Lakshika Parihar <31981299+lakshika1064@users.noreply.github.com>
2021-01-25 17:01:44 +0530

    and_or_not_True_False

---
+++ Python/Keywords/and_or_not_True_False.py
@@ -0,0 +1,6 @@
+# and returns true if both the operands are true else it returns false
+print(True and False)
+# or returns true if any of the operands are true else it returns false
+print(True or False)
+# not returns the opposit True--> False   False--> True
+print(not(True))
