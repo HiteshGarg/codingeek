d385b3a7bb3583a8ccb6d9772e1e74b85ff857bd
Lakshika Parihar <31981299+lakshika1064@users.noreply.github.com>
2021-01-26 17:16:29 +0530

    Update if_elif_else_for_in.py

--- Python/Keywords/if_elif_else_for_in.py
+++ Python/Keywords/if_elif_else_for_in.py
@@ -1,7 +1,7 @@
 for i in range(10):
     if i%2==0:                        #checks the condition if true then print statement otherwise go to else condition
         print("Multiple of 2")
-    elif i%3==0:                      #when if condition fails we come to elif where we can again checks the condition and move forward
+    elif i%3==0:                      #when if condition fails we come to elif where we can again check the condition and move forward
         print("Multiple of 3")
-    else:                             # if we reached here that means if condition was false and we will print the statement
+    else:                            # if we reached here that means if condition was false and we will print the statement
         print("Not a multiple of either 2 or 3")
