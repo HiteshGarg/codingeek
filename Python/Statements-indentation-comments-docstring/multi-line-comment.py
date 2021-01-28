eb400d74081c9c9d706aa369246f3b662d8233b0
Lakshika Parihar <31981299+lakshika1064@users.noreply.github.com>
2021-01-28 13:13:00 +0530

    Update multi-line-comment.py

--- Python/Statements-indentation-comments-docstring/multi-line-comment.py
+++ Python/Statements-indentation-comments-docstring/multi-line-comment.py
@@ -2,3 +2,13 @@
 # Author: Codingeek
 # Description: In this program, we are printing a quote
 print("It's a beautiful day to save lives")
+
+"""Date-of-creation : 27-01-2021
+    Author: Codingeek
+    Description: In this program, we are printing a quote"""
+print("It's a beautiful day to save lives")
+
+'''Date-of-creation : 27-01-2021
+    Author: Codingeek
+    Description: In this program, we are printing a quote'''
+print("It's a beautiful day to save lives")
