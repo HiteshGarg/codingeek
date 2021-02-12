d052f5ce2fb47b1f40c9c26ff7af4f7dfc6c8555
Hitesh Kumar Garg <HiteshGarg@users.noreply.github.com>
2021-02-12 09:19:32 +0100

    Update Add_Sub.c

--- C-Tutorials/pointers/PointerOperations/Add_Sub.c
+++ C-Tutorials/pointers/PointerOperations/Add_Sub.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
-int main()
-{
-    int a=5,*x;//declaring the pointer for integer variable
-    char b='z',*y;//declaring the pointer for char variable
-    x=&a;//storing the memory location of variable a in pointer variable x
-    /*The corresponding values of the addition and subtraction operations on pointer variable x are given below*/
-    printf("x= %d\n",x);//printing the actual value of x
-    printf("x+3= %d\n",x+3);//the value incremented by 3
-    printf("x-2= %d\n",x-2);//the value decremented by 2
-    y=&b;//storing the memory location of variable b in pointer variable y
-    /*The corresponding values of the addition and subtraction operations on pointer variable y are given below*/
-    printf("y= %d\n",y);//printing the actual value of y
-    printf("y+3= %d\n",y+3);//the value incremented by 3
-    printf("y-2= %d\n",y-2);//the value decremented by 2
-    return 0;
+
+int main() {
+  //declaring the pointer for integer variable
+  int a = 5, * x;
+
+  //declaring the pointer for char variable
+  char b = 'z', * y;
+
+  //storing the memory location of variable a in pointer variable x
+  x = & a;
+
+  /*The corresponding values of the addition and subtraction operations on pointer variable x are given below*/
+  //printing the actual value of x
+  printf("x= %d\n", x);
+
+  //the value incremented by 3
+  printf("x+3= %d\n", x + 3);
+
+  //the value decremented by 2
+  printf("x-2= %d\n", x - 2);
+
+  //storing the memory location of variable b in pointer variable y
+  y = & b;
+
+  /*The corresponding values of the addition and subtraction operations on pointer variable y are given below*/
+  //printing the actual value of y
+  printf("y= %d\n", y);
+
+  //the value incremented by 3
+  printf("y+3= %d\n", y + 3);
+
+  //the value decremented by 2
+  printf("y-2= %d\n", y - 2);
+  return 0;
 }
