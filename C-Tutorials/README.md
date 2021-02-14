982c99c07b67d8afcab98416c7be37ed1696b825
Hitesh kumar Garg <hiteshkumar.garg@otto.de>
2021-02-14 22:15:54 +0100

    Print Nth Fibonacci number in C programming language

--- C-Tutorials/README.md
+++ C-Tutorials/README.md
@@ -47,3 +47,4 @@
 - [“Hello World” Program in C](https://www.codingeek.com/tutorials/c-programming/example/print-hello-world/)
 - [How to Print an Integer, long, double entered by User in a C Program?](https://www.codingeek.com/tutorials/c-programming/example/print-integer-long-double-user-input/)
 - [C Program to find Sum of first ‘n’ Natural numbers](https://www.codingeek.com/tutorials/c-programming/example/find-sum-first-n-natural-numbers/)
+- [C Program to Print Nth Fibonacci number](https://www.codingeek.com/tutorials/c-programming/example/print-nth-fibonacci-number/)
---
+++ C-Tutorials/examples/basic/NthFibonacciNumber.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+long fibonacci(int num)
+{
+
+    long a = 0, b = 1, c = 0;
+    if (num <= 1)
+        return num;
+    for (int i = 2; i <= num; i++)
+    {
+        c = a + b; // stores previous 2 values of the series i.e. a and b
+        a = b;     //copy previous number to its previous number
+        b = c;     //copy sum (c) to first previous number
+    }
+    return b;
+}
+
+long fibonacciRecursive(int num)
+{
+    if (num <= 1)
+        return num;
+    return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("The %dth fibonacci number is %ld", num, fibonacci(num));
+    printf("\nThe %dth fibonacci number is %ld", num, fibonacciRecursive(num));
+    return 0;
+}
