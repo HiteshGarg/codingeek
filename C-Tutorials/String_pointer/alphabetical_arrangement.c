0ec0d11a8dfd52f94bbe523978abd0b9b4ae3754
Hitesh kumar Garg <hiteshkumar.garg@otto.de>
2021-02-24 07:07:00 +0100

    Rearrange String pointer code in C

--- C-Tutorials/String_pointer/alphabetical_arrangement.c
+++ C-Tutorials/String_pointer/alphabetical_arrangement.c
@@ -1,34 +1,35 @@
-#include <stdio.h>
 #include <String.h>
-int main()
-{
-	//initializing the pointer string array
-    char *names[]={
-                    "tree",
-                    "bowl",
-                    "hat",
-                    "mice",
-                    "toon"
-                    };
-    char *temp;//temporary variable for swaping the values
-    int i,j,a;
-    printf("The names are:\n");
-    for(i=0 ;i<5 ;i++ )
-        printf("%s\n",names[i]);
-    //arranging names in alphabetically using selection sort
-    for(i=0 ;i<5 ;i++ ){
-        for(j=i+1 ;j<5 ;j++ ){
-            a=strcmp(names[i],names[j]);//compares the two string and returns an integer value
-			//if the value of a is greater than 0 then swapping begins
-            if(a>0){
-                temp=names[i];
-                names[i]=names[j];
-                names[j]=temp;
-            }
-        }
+#include <stdio.h>
+int main() {
+  // initializing the pointer string array
+  char *names[] = {"tree", "bowl", "hat", "mice", "toon"};
+  char *temp; // temporary variable for swaping the values
+  int i, j, a;
+
+  printf("The names are:\n");
+
+  for (i = 0; i < 5; i++)
+    printf("%s\n", names[i]);
+
+  // arranging names in alphabetically using selection sort
+  for (i = 0; i < 5; i++) {
+    for (j = i + 1; j < 5; j++) {
+      // compares the two string and returns an integer value
+      // if the value of a is greater than 0 then swapping begins
+      a = strcmp(names[i], names[j]);
+      
+      if (a > 0) {
+        temp = names[i];
+        names[i] = names[j];
+        names[j] = temp;
+      }
     }
-    printf("The arranged names are:\n");
-    for(i=0 ;i<5 ;i++ )
-        printf("%s\n",names[i]);
-    return 0;
+  }
+
+  printf("The arranged names are:\n");
+
+  for (i = 0; i < 5; i++)
+    printf("%s\n", names[i]);
+
+  return 0;
 }
--- C-Tutorials/String_pointer/drawback_solution.c
+++ C-Tutorials/String_pointer/drawback_solution.c
@@ -1,24 +1,31 @@
-#include<stdio.h>
-#include<String.h>
-#include<stdlib.h>
-int main()
-{
-	char *names[5],a[5];//declaring the string pointer array as well as a char array
-	int l,i;
-	char *x;
-	printf("Enter 5 strings:\n");
-	/*taking inputs in char array as well as copying them to the string pointer array*/
-	for(i=0 ;i<5 ;i++ )
-	{
+#include <String.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+  // declaring the string pointer array as well as a char array
+  char *names[5], a[5];
+  int l, i;
+  char *x;
 
-		scanf("%s",a);//taking values in char array
-		l=strlen(a);
-		x=(char*)malloc(l+1);//used malloc to allocate dynamic memory. l+1 to store "\0". 
-		strcpy(x,a);
-		names[i]=x;
-	}
-        printf("The strings are:\n");
-	for(i=0 ;i<5 ;i++ )
-		printf("%s\n",names[i]);
+  printf("Enter 5 strings:\n");
+
+  // taking inputs in char array as well
+  // as copying them to the string pointer array
+  for (i = 0; i < 5; i++) {
+
+    scanf("%s", a); // taking values in char array
+    l = strlen(a);
+
+    // used malloc to allocate dynamic memory. l+1 to store "\0".
+    x = (char *)malloc(l + 1);
+    strcpy(x, a);
+    names[i] = x;
+  }
+
+  printf("The strings are:\n");
+  
+	for (i = 0; i < 5; i++)
+    printf("%s\n", names[i]);
+  
 	return 0;
 }
