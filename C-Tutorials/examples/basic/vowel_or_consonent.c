15a1df283fd3973721d35d88c73ad332a4db7b07
Hitesh kumar Garg <hiteshkumar.garg@otto.de>
2021-02-28 15:33:56 +0100

    vowel or consonant

---
+++ C-Tutorials/examples/basic/vowel_or_consonent.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void checkIfVowelOrConsonant(char ch) {
+  if (ch == 'a' || ch == 'A' || 
+      ch == 'e' || ch == 'E' || 
+      ch == 'i' || ch == 'I' ||
+      ch == 'o' || ch == 'O' ||
+      ch == 'u' || ch == 'U')
+    printf("%c Is a vowel.\n", ch);
+  else
+    printf("%c Is a consonant.\n", ch);
+}
+// Using Switch case
+// void checkIfVowelOrConsonant(char ch) {
+//   switch (ch) {
+//   case 'a':
+//   case 'A':
+//   case 'e':
+//   case 'E':
+//   case 'i':
+//   case 'I':
+//   case 'o':
+//   case 'O':
+//   case 'u':
+//   case 'U':
+//     printf("%c is a vowel.\n", ch);
+//     break;
+//   default:
+//     printf("%c is a Consonant.\n", ch);
+//   }
+
+
+int main() {
+  char ch;
+  printf("Enter a character\n");
+  scanf("%c", &ch);
+
+  checkIfVowelOrConsonant(ch);
+  return 0;
+}
