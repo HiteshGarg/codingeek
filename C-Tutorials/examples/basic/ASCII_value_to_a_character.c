79b5520f06b7b50db1519bdb1095b2798df33dc8
Hitesh kumar Garg <hiteshkumar.garg@otto.de>
2021-03-08 14:35:04 +0100

    Refactor C code and Python readme

---
+++ C-Tutorials/examples/basic/ASCII_value_to_a_character.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  // change to user input if needed
+  char ch = 'A'; 
+  int ascii = 65; // Ascii value of 'A'
+  char from_ascii = ascii;
+  printf("ch to Ascii = %d", ch);
+  printf("\nascii to char = %c", ascii);
+  printf("\nascii to char variable = %c", from_ascii);
+  return 0;
+}
