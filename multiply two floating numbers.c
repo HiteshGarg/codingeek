b98ef7f2e7225595ec3f1e3cdfc3bfcfd41c7768
Arushi Dwivedi <50775209+aru26@users.noreply.github.com>
2021-03-22 18:55:36 +0530

    Create  multiply two floating numbers.c

---
+++ multiply two floating numbers.c
@@ -0,0 +1,32 @@
+//using standard method
+#include <stdio.h>
+int main(){
+  float number1, number2, Product;
+  printf("Enter the first Number: ");
+  scanf("%f", &number1);
+  printf("Enter the second Number: ");
+  scanf("%f", &number2);
+
+  Product = number1 * number2;
+
+  printf("The Product of the numbers is :%.3f", Product);
+  return 0;
+}
+
+//using function
+#include <stdio.h>
+float multiply(float x, float y){
+    return x * y;
+}
+int main(){
+  float number1, number2, Product;
+  printf("Enter the first Number: ");
+  scanf("%f", &number1);
+  printf("Enter the second Number: ");
+  scanf("%f", &number2);
+
+  Product  = multiply(number1, number2);
+
+  printf("The Product of the numbers is :%.3f", Product);
+  return 0;
+}
